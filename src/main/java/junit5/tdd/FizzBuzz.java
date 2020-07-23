a6ff4e5a34c3162a0648dfea8692d5c4fab32ba4
OOCLDM\lisa8 <lisa8@oocl.com>
2020-07-23 12:47:21 +0800

    [FizzBuzz][Sam] refactor: pass should_return_15_when_counting_off_given_the_multiples_of_3_and_5

--- src/main/java/junit5/tdd/FizzBuzz.java
+++ src/main/java/junit5/tdd/FizzBuzz.java
@@ -3,6 +3,9 @@ package junit5.tdd;
 public class FizzBuzz {
 
     public String countOff(int number) {
+        if ((number % 3 == 0) && (number % 5 == 0)) {
+            return "FizzBuzz";
+        }
 
         if (number % 3 == 0) {
             return "Fizz";
